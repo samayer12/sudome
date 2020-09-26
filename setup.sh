6c90b05bf99e775e7408e5cc59a3c2beee96523b
Sam Mayer <samayer12@gmail.com>
2020-09-26 13:45:01 -0500

    Install requirements during setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,16 @@ echo "$(whoami) $(hostname) = (root) NOPASSWD: $(which python)" | sudo EDITOR='t
 echo "INFO - File contents: /etc/sudoers.d/python"
 sudo cat /etc/sudoers.d/python
 
+if ! command -v virtualenv &>/dev/null; then
+  echo "ERROR - Missing virtualenv binary. Run: pip install virtualenv"
+  exit 1
+fi
+
+# Create and enter venv
+virtualenv venv
+source venv/bin/activate
+# Update pip and gather requirements
+python -m pip install -U pip
+pip install -r requirements.txt
+
 echo -e "\nNext, add \`python-sudo.sh\` as an existing-environment interpreter in PyCharm."
