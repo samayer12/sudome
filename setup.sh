ae321615d3d6fd80f19ad67158b1c71abf51b3eb
Sam Mayer <samayer12@gmail.com>
2020-09-26 13:45:29 -0500

    Format informational messages

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 # Generate config line to execute virtualenv from Pycharm.
-echo "Generate configuration line:"
-echo "$(whoami) $(hostname) = (root) NOPASSWD: $(which python)" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/python
+echo "INFO  - Generate configuration line:"
+# echo "$(whoami) $(hostname) = (root) NOPASSWD: $(which python)" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/python
 
-# Confirm changes to file.
-echo "INFO - File contents: /etc/sudoers.d/python"
+echo "INFO  - File contents: /etc/sudoers.d/python"
 sudo cat /etc/sudoers.d/python
 
 if ! command -v virtualenv &>/dev/null; then
